92fedb1058cd9a334c0f2d49f2863de874a37b61
awauters <arne.wauters99@gmail.com>
2020-10-26 16:32:28 +0100

    fixed errors, I assume at least

--- first-app/first-app/app.component.spec.ts
+++ first-app/first-app/app.component.spec.ts
@@ -1,29 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { appComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        appComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(appComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'first-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(appComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('first-app');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(appComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('first-app app is running!');
